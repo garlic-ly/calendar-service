73b5d9f071978f3d9ee6a2d1f6aaf2fd49896068
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-06-20 13:11:02 -0700

    Updates Vars to Lets and Consts

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-var db = require('../db/index.js');
+const db = require('../db/index.js');
 const port = 3001;
 
 
@@ -8,18 +8,18 @@ app.use(express.static(__dirname + '/../client/dist'));
 
 // GET Route
 app.get('/api/rooms/:roomId', (req, res) => {
-  var roomId = req.params.roomId;
-  var roomQuery = 'SELECT * FROM rooms WHERE id = ?';
+  let roomId = req.params.roomId;
+  let roomQuery = 'SELECT * FROM rooms WHERE id = ?';
   db.connection.query(roomQuery, [roomId], (error, roomResults, fields) => {
     if (error) {
       res.status(404).send(error);
     }
-    var resQuery = 'SELECT resId, startDate, endDate FROM reservations WHERE roomId = ?'
+    let resQuery = 'SELECT resId, startDate, endDate FROM reservations WHERE roomId = ?'
     db.connection.query(resQuery, [roomId], (error, resResults, fields) => {
       if (error) {
         res.status(404).send(error);
       }
-      var results = roomResults.concat(resResults);
+      let results = roomResults.concat(resResults);
       res.status(200).send(results);
     });
   });
