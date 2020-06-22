c09d30c5e33ef17052b0bb1ffa15e13cbb784c70
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-06-22 12:04:37 -0700

    Updates Tests for Guest and Guest Dropdown
    This commit updates the tests for Guest and Guest Dropdown so that
    they continue to work since I updated the components they are testing.

--- client/src/__test__/guest.test.js
+++ client/src/__test__/guest.test.js
@@ -8,14 +8,32 @@ Enzyme.configure({adapter: new Adapter()});
 
 describe('Tests for Guest Component', () => {
   it('Guest renders without crashing', () => {
-    const wrapper = shallow(<Guest />);
+    const mockCallBack = jest.fn();
+    const options = {
+      dropdownOpen: false,
+      guestMenuToggle: mockCallBack,
+      guestCount: 1,
+      updateGuestCount: mockCallBack,
+      adults: 0,
+      childrenCount: 0,
+      infants: 0,
+    }
+    const wrapper = shallow(<Guest {...options}/>);
     expect(wrapper.exists()).toBe(true);
   });
 
   it('Tests click event', () => {
     const mockCallBack = jest.fn();
-
-    const guest = shallow((<Guest guestMenuToggle={mockCallBack}/>));
+    const options = {
+      dropdownOpen: false,
+      guestMenuToggle: mockCallBack,
+      guestCount: 1,
+      updateGuestCount: mockCallBack,
+      adults: 0,
+      childrenCount: 0,
+      infants: 0,
+    }
+    const guest = shallow((<Guest {...options}/>));
     guest.find('.guest').simulate('click');
     expect(mockCallBack.mock.calls.length).toEqual(1);
   });
--- client/src/__test__/guestDropdown.test.js
+++ client/src/__test__/guestDropdown.test.js
@@ -8,15 +8,31 @@ Enzyme.configure({adapter: new Adapter()});
 
 describe('Tests for Guest Dropdown Component', () => {
   it('GuestDropdown renders without crashing', () => {
-    const wrapper = shallow(<GuestDropdown />);
+    const mockCallBack = jest.fn();
+    const options = {
+      updateGuestCount: mockCallBack,
+      adults: 1,
+      childrenCount: 0,
+      infants: 0,
+    }
+    const wrapper = shallow(<GuestDropdown {...options}/>);
     expect(wrapper.exists()).toBe(true);
   });
 
   it('Tests click event', () => {
-    const mockCallBack = jest.fn();
-
-    const guest = shallow((<GuestDropdown updateGuestCount={mockCallBack}/>));
+    const mockCallBack = jest.fn(() => {
+      options.adults = options.adults + 1;
+    });
+    const options = {
+      updateGuestCount: mockCallBack,
+      adults: 1,
+      childrenCount: 0,
+      infants: 0,
+    }
+    const guest = shallow((<GuestDropdown {...options}/>));
+    expect(options.adults).toBe(1);
     guest.find('.adults-add').simulate('click');
     expect(mockCallBack.mock.calls.length).toEqual(1);
+    expect(options.adults).toBe(2);
   });
 });
