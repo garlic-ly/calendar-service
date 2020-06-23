b420cd8c71940f2ab95d57bb15d8c087a272a309
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-06-23 11:57:51 -0700

    Refactors Calendar Component to State Component
    Due to the amount of data needed to keep track of moving between months
    I decided that this needs to be a state component. At this time I have
    refactor the existing code and moving into the ability to move months.

--- client/src/components/calendar.jsx
+++ client/src/components/calendar.jsx
@@ -2,79 +2,119 @@ import React from 'react';
 import moment from 'moment';
 import styled from 'styled-components';
 
-const Calendar = (props) => {
-  const weekdays = moment.weekdaysMin();
-  const months = moment.months();
-  // Get Month, Date, Year, Num of Days
-  const month = () => {
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      leftCalendarMoment: moment(),
+      rightCalendarMoment: moment().add(1, 'M'),
+      weekdays: moment.weekdaysMin(),
+      months: moment.months(),
+    };
+    this.month = this.month.bind(this);
+    this.weekHeader = this.weekHeader.bind(this);
+    this.allDates = this.allDates.bind(this);
+    this.blankDates = this.blankDates.bind(this);
+    this.monthDates = this.monthDates.bind(this);
+    this.today = this.today.bind(this);
+    this.year = this.year.bind(this);
+    this.daysInMonth = this.daysInMonth.bind(this);
+    this.monthStart = this.monthStart.bind(this);
+    this.monthArray = this.monthArray.bind(this);
+  }
+
+  // Get Month, Date, Year, Num of Days per month, etc
+  month() {
     return moment().format('MMMM');
   }
-  const today = () => {
+
+  today() {
     return moment().get('date');
   }
-  const year = () => {
+
+  year() {
     return moment().format('Y');
   }
-  const daysInMonth = () => {
+
+  daysInMonth() {
     return moment().daysInMonth();
   }
-  const weekHeader = () => {
-    return weekdays.map((weekday) => {
-      return <td>{weekday}</td>
-    });
-  }
-  const monthStart = () => {
+
+  monthStart() {
     return (moment().startOf('month').format('d'));
   }
-  const blankDates = () => {
-    let numOfBlanks = monthStart();
-    let totalBlanks = [];
+
+  // Create Arrays of empty dates, actual month days, and a combination of both
+  blankDates() {
+    const numOfBlanks = this.monthStart();
+    const totalBlanks = [];
     for (let i = 0; i < numOfBlanks; i += 1) {
       totalBlanks.push(' ');
     }
     return totalBlanks;
   }
-  const allDates = () => {
-    let blank = blankDates();
-    let numOfDays = daysInMonth();
-    let daysOfMonth = [];
+
+  monthDates() {
+    const numOfDays = this.daysInMonth();
+    const daysOfMonth = [];
     for (let i = 1; i <= numOfDays; i += 1) {
       daysOfMonth.push(i);
     }
-    let totalDays = blank.concat(daysOfMonth);
-    console.log(totalDays);
+    return daysOfMonth;
+  }
+
+  monthArray() {
+    const blank = this.blankDates();
+    const trueDays = this.monthDates();
+    const totalDays = blank.concat(trueDays);
     let week = [];
-    let month = [];
+    const month = [];
     for (let j = 0; j < totalDays.length; j += 1) {
-      week.push(totalDays[j])
+      week.push(totalDays[j]);
       if (week.length === 7 || totalDays.length - 1 === j) {
         month.push(week);
         week = [];
       }
     }
-    return month.map((innerArr) => {
-      return (
-        <tr>
-          {innerArr.map((day) => {
-            return <td>{day}</td>
-          })}
-        </tr>
-      )
-    });
+    return month;
   }
 
-  return(
-    <table>
-      <thead>
-        <tr>
-          {weekHeader()}
-        </tr>
-      </thead>
-      <tbody>
-        {allDates()}
-      </tbody>
-    </table>
-  );
-};
+  // Render Functions
+  weekHeader() {
+    const { weekdays } = this.state;
+    return weekdays.map((weekday) => <td key={weekday}>{weekday}</td>);
+  }
+
+  allDates() {
+    const month = this.monthArray();
+    return month.map((singleWeek, weekIndex) => (
+      <tr key={weekIndex}>
+        {singleWeek.map((oneDay, dayIndex) => <td key={dayIndex}>{oneDay}</td>)}
+      </tr>
+    ));
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{ display: 'flex' }, { justifyContent: 'space-between' }}>
+          <button> B </button>
+          <span>{this.month()}</span>
+          <button> F </button>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              {this.weekHeader()}
+            </tr>
+          </thead>
+          <tbody>
+            {this.allDates()}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 export default Calendar;
