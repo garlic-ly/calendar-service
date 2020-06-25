8347fa2b7386db4d70fa776ef301b5342686d0a8
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-06-25 16:02:09 -0700

    Adds Small Changes to SVG Files
    Corrects spacing and property naming on elements.

--- client/src/components/icons/rightArrowSVG.jsx
+++ client/src/components/icons/rightArrowSVG.jsx
@@ -10,9 +10,8 @@ const ArrowSVG = styled.svg`
 
 const RightArrowSVG = () => (
   <ArrowSVG viewBox="0 0 18 18" role="presentation" aria-hidden="true" focusable="false">
-    <path d="m4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z" fill-rule="evenodd"></path>
+    <path d="m4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z" fillRule="evenodd"></path>
   </ArrowSVG>
 );
 
 export default RightArrowSVG;
-
